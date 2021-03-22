5bc8c2b2190438f65f84c1dac1db9a5c82c18bcd
PrommarinPromngam <80934340+PrommarinPromngam@users.noreply.github.com>
2021-03-22 22:06:29 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1615981370,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bg.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/thrush-sheet0.png",
+		"images/thrush-sheet1.png",
+		"images/thrush-sheet2.png",
+		"images/bullet-sheet0.png",
+		"images/rock-sheet0.png",
+		"images/particles.png",
+		"images/spawner-sheet0.png",
+		"images/bg2-sheet0.png",
+		"images/particles2.png",
+		"media/the imperial march-darth vaders theme (from star wars the empire strikes back) (online-audio-converter.com).m4a",
+		"media/the imperial march-darth vaders theme (from star wars the empire strikes back) (online-audio-converter.com).ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
